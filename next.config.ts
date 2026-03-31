b92b5b31846b9d1bc62da5cd5b1db8c1f70c28a7
Veltrixcoder <veltrix620@gmail.com>
2026-03-31 15:34:43 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -11,18 +11,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/.well-known/assetlinks.json',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
