12c4809d7c380615f1f247d384588912572ebf10
Veltrixcoder <veltrix620@gmail.com>
2026-03-31 14:59:07 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -23,14 +23,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/.well-known/assetlinks.json',
-        destination: '/api/assetlinks',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
